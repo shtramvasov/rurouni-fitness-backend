c658c4f329034a2085ddf34cd8f704681bf4bdf8
Rurouni Kenshin <stramvasov@gmail.com>
2025-06-29 21:08:36 +0500

    1

--- routes/files/files.js
+++ routes/files/files.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const supabase = require('../../fs-connection')
-const sharp = require('sharp')
 
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -12,20 +11,13 @@ router.post('/', upload.single('avatar'), async (req, res) => {
   try {
     if (!file) return res.status(400).json({ message: 'Не передан файл' });
 
-    // Оптимизируем изображение с помощью sharp
-    const optimizedImage = await sharp(file.buffer)
-      .resize(800, 800, { fit: 'inside', withoutEnlargement: true})
-      .jpeg({ quality: 80, progressive: true })
-      .toBuffer()
-    ;
-
-    // Генерируем уникальное имя файла (например, userID + timestamp)
-    const fileName = `avatars/${Date.now()}.jpg`;
+    // Генерируем уникальное имя файла
+    const fileName = `avatars/${Date.now()}`;
 
     // Загружаем на FS
     const { error } = await supabase.storage
       .from('rurouni-fs')
-      .upload(fileName, optimizedImage, { contentType: 'image/jpeg', upsert: true})
+      .upload(fileName, file.buffer, { contentType: file.mimetype, upsert: true})
     ;
 
     if (error) return res.status(500).json({ message: 'Ошибка загрузки файла' });
